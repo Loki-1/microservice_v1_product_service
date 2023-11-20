59514da51811feab732ce21f3cd95fe19fca56fc
Lokesh Naagam <134843197+Loki-1@users.noreply.github.com>
2023-11-20 16:40:32 +0530

    Update ProductResponse.java

--- src/main/java/com/dailycodebuffer/productservice/model/ProductResponse.java
+++ src/main/java/com/dailycodebuffer/productservice/model/ProductResponse.java
@@ -1,19 +1,5 @@
 package com.dailycodebuffer.productservice.model;
 
-import lombok.Data;
-
-@Data
-public class ProductRequest {
-    private String name;
-    private long price;
-    private long quantity;
-
-}
-[root@ip-172-31-5-21 model]# ls
-ErrorResponse.java  ProductRequest.java  ProductResponse.java
-[root@ip-172-31-5-21 model]# cat ProductResponse.java
-package com.dailycodebuffer.productservice.model;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
